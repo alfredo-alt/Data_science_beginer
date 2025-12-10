006c0fe11a3bbcae473cd2996be256018fb12613
alfredo minchez <aminchez@uni.edu.pe>
2025-12-09 21:27:03 -0500

    classes

---
+++ semana1/4_bucle.py
@@ -0,0 +1,7 @@
+#bucle for
+for contador in range(1,3):
+    print(contador)
+tabla = int(input('que tabla de multiplicar deseas ver '))
+#bucle for
+for contador in range(1,11):
+    print(f"{tabla}x{contador}={tabla*contador}")
---
+++ semana1/dia1.py
@@ -0,0 +1,3 @@
+nombre = 'alfredo'
+edad = input('cual es tu edad') 
+print('hola ', nombre, 'cuya edad es ', edad)
